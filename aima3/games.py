44b7481be270fa01830996b4b3086f82b0ba0c44
Douglas Blank <doug.blank@gmail.com>
2018-02-18 10:27:07 -0500

    Release 1.0.10

--- aima3/games.py
+++ aima3/games.py
@@ -452,8 +452,8 @@ class ConnectFour(TicTacToe):
     row, or in a square directly above an occupied square.  Traditionally
     played on a 7x6 board and requiring 4 in a row."""
 
-    def __init__(self, h=7, v=6, k=4):
-        TicTacToe.__init__(self, h, v, k)
+    def __init__(self, h=7, v=6, k=4, *args, **kwargs):
+        TicTacToe.__init__(self, h, v, k, *args, **kwargs)
 
     def actions(self, state):
         return [(x, y) for (x, y) in state.moves
